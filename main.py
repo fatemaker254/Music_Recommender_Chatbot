f7e7381469b50f48de29ca4c0faa1a2fdeaf1359
fatemaker254 <dassoham254@gmail.com>
2024-02-22 12:15:40 +0530

    Version2.0

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 import pandas as pd
 import joblib
@@ -5,6 +6,11 @@ import joblib
 st.title("Music Recommender Chatbot")
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
+st.write(
+    "<font size='+6'>🤖 : Hi, What song do you want to listen today?</font>",
+    unsafe_allow_html=True,
+)
+
 # Load the saved model
 tfidf_vectorizer = joblib.load("tfidf_vectorizer.joblib")
 cosine_sim = joblib.load("cosine_similarity.joblib")
@@ -12,6 +18,41 @@ cosine_sim = joblib.load("cosine_similarity.joblib")
 # Load the dataset
 df = pd.read_csv("Spotify_Youtube.csv")
 
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("image.jpg")
+# adding background image
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image:url("data:image/jpg;base64,{img}");
+background-size: 100%;
+background-position: top left;
+background-repeat: repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+[data-testid="sttextArea"] {{
+color: white;
+}}
+[data-testid="sttext_input] {{
+color: black; /* Change text color to black */
+background-color: white; /* Change background color to white */
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
 # Initialize session_state if not exists
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -35,7 +76,6 @@ def recommend_songs(song_name, cosine_sim, df, tfidf_vectorizer):
 
 # Sidebar with a button to delete chat history
 
-
 # Display chat messages
 for message in st.session_state.messages:
     avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
@@ -45,13 +85,15 @@ for message in st.session_state.messages:
 # Main chat interface
 if prompt := st.text_input("You:", key="user_input"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    capitalized_string = prompt.title()
 
     with st.chat_message("assistant", avatar=BOT_AVATAR):
-        recommendations = recommend_songs(prompt, cosine_sim, df, tfidf_vectorizer)
-        if recommendations.empty:
-            message_content = (
-                f"No matching song found for '{prompt}'. Please try another song."
-            )
+        recommendations = recommend_songs(
+            capitalized_string, cosine_sim, df, tfidf_vectorizer
+        )
+        message_content = ""
+        if isinstance(recommendations, str):
+            message_content = recommendations
         else:
             message_content = "\n".join(
                 [
